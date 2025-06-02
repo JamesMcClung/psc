f7d7011e3d1609ac3d306c8f2ab2f5c8d969a66a
James McClung <james@themcclungs.net>
2025-06-02 13:18:56 -0400

    injector_boundary_inflow: rm ip

--- src/include/injector_boundary_inflow.hxx
+++ src/include/injector_boundary_inflow.hxx
@@ -61,7 +61,6 @@ public:
   using Mparticles = typename PushParticles::Mparticles;
   using MfieldsState = typename PushParticles::MfieldsState;
   using AdvanceParticle_t = typename PushParticles::AdvanceParticle_t;
-  using InterpolateEM_t = typename PushParticles::InterpolateEM_t;
   using Current = typename PushParticles::Current;
   using Dim = typename PushParticles::Dim;
   using real_t = typename PushParticles::real_t;
@@ -81,7 +80,6 @@ public:
 
     PI<real_t> pi(grid);
     Real3 dxi = Real3{1., 1., 1.} / Real3(grid.domain.dx);
-    InterpolateEM_t ip;
     Current current(grid);
     auto accessor = mprts.accessor_();
 
@@ -131,7 +129,6 @@ public:
             // Taken from push_particles_1vb.hxx PushParticlesVb::push_mprts()
 
             Real3 initial_normalized_pos = initial_x * dxi;
-            ip.set_coeffs(initial_normalized_pos);
 
             Int3 final_idx;
             Real3 final_offset;
